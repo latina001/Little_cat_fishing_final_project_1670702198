a854b1cff5c04733fccefcab5a3bad17fc6d0852
Lin <nannaphat20160@gmail.com>
2025-11-24 10:41:15 +0700

    ทำ สปอน เล่นดูคร่าวๆ

--- Assets/Object Game/Script/GameManager.cs
+++ Assets/Object Game/Script/GameManager.cs
@@ -1,6 +1,31 @@
 using UnityEngine;
 
-public class GameManager
+public class GameManager : MonoBehaviour
 {
-    
+    public GameObject[] fishPrefabs;
+    public Transform[] spawnPoints;
+
+    public float spawnInterval = 2f;
+    private float timer;
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if (timer >= spawnInterval)
+        {
+            SpawnFish();
+            timer = 0f;
+        }
+    }
+
+    void SpawnFish()
+    {
+        int fishIndex = Random.Range(0, fishPrefabs.Length);
+        int spawnIndex = Random.Range(0, spawnPoints.Length);
+
+        Instantiate(fishPrefabs[fishIndex],
+                    spawnPoints[spawnIndex].position,
+                    Quaternion.identity);
+    }
 }
