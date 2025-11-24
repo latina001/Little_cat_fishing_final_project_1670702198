56f9f43948a25fbbb22ca2cd2a27184d1cdd5703
Lin <nannaphat20160@gmail.com>
2025-11-24 20:28:10 +0700

    up date text code

--- Assets/Object Game/Script/BoundaryRemover.cs
+++ Assets/Object Game/Script/BoundaryRemover.cs
@@ -2,22 +2,21 @@ using UnityEngine;
 
 public class BoundaryRemover : MonoBehaviour
 {
-    // ต้องลาก GameObject ที่มี MiniGame.cs มาใส่ใน Inspector
+    
     public MiniGame miniGameManager;
 
     void OnTriggerEnter2D(Collider2D other)
     {
-        // ตรวจสอบ Tag ว่าเป็น "Fish" 
+        
         if (other.CompareTag("Fish"))
         {
             if (miniGameManager != null)
             {
                
 
-                // 2. ทำลายปลา
                 Destroy(other.gameObject);
 
-                // 3. เรียกฟังก์ชันปิดเกม
+               
                 miniGameManager.LoseMiniGame();
             }
         }
